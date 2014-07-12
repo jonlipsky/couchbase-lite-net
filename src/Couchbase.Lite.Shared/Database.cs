5c6e48bde50b93c5d7c40490d32c0aa530fdb85e
Jon Lipsky <jon@elevenworks.com>
2014-07-12 02:01:29 -0500

    More fixes.

--- src/Couchbase.Lite.Shared/Database.cs
+++ src/Couchbase.Lite.Shared/Database.cs
@@ -3634,7 +3634,7 @@ PRAGMA user_version = 3;";
                                                              BadAttachment);
                         }
 
-                        var revPos = ((int)attachInfo.Get("revpos"));
+						var revPos = Convert.ToInt32(attachInfo.Get("revpos"));
                         if (revPos <= 0)
                         {
                             throw new CouchbaseLiteException("Invalid revpos: " + revPos, StatusCode.BadAttachment
